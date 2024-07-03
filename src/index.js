498154c53830774213ce69228f6dcfa729cb5ccf
[[alvarobernal2412]] <alvarobc2412@gmail.com>
2024-07-03 18:26:43 +0200

    fix: Allow all origins

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "*",
         credentials: true,
         allowedHeaders: ["Content-Type", "Authorization", "Set-Cookie"],
     })
