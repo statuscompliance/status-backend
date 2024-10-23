995544c39f6eed808e87479168597afa466e220f
[[alvarobernal2412]] <alvarobc2412@gmail.com>
2024-10-23 17:03:27 +0200

    fix: Remove tpa endpoints

--- src/controllers/catalog.controller.js
+++ src/controllers/catalog.controller.js
@@ -1,4 +1,4 @@
-import models from '../../db/models.js'
+import models from "../../db/models.js";
 import fs from "fs/promises";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
@@ -8,115 +8,75 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 export const getCatalogs = async (req, res) => {
     const catalogs = await models.Catalog.findAll();
     res.json(catalogs);
-}
+};
 
 export const getCatalog = async (req, res) => {
     const row = await models.Catalog.findByPk(req.params.id);
 
     if (!row)
         return res.status(404).json({
-          message: "Catalog not found",
+            message: "Catalog not found",
         });
-        
+
     res.json(row);
-}
+};
 
 export const createCatalog = async (req, res) => {
-    const {name,startDate,endDate,dashboard_id} = req.body
+    const { name, startDate, endDate, dashboard_id } = req.body;
     const rows = await models.Catalog.create({
         name,
         startDate,
         endDate,
-        dashboard_id
+        dashboard_id,
     });
     res.send({
         id: rows.id,
         name,
         startDate,
         endDate,
-        dashboard_id
-    })
-}
-
-export const getTpa = async (req, res) => {
-    const { catalog_id } = req.params;
-    const filePath = join(__dirname, "../tpa", `tpa-${catalog_id}.json`);
-  
-    try {
-      const content = await fs.readFile(filePath, "utf8");
-      res.json(JSON.parse(content));
-    } catch (err) {
-      console.error("Error al leer el archivo:", err);
-      res.status(500).send("Error al cargar el TPA");
-    }
+        dashboard_id,
+    });
 };
-
-export const saveTpa = async (req, res) => {
-    const { content, catalog_id } = req.body;
-    const filePath = join(__dirname, "../tpa", `tpa-${catalog_id}.json`);
-  
-    try {
-      await fs.writeFile(filePath, content, "utf8");
-      res.send("TPA guardado con éxito");
-    } catch (err) {
-      console.error("Error al escribir el archivo:", err);
-      res.status(500).send("Error al guardar el TPA");
-    }
-};
-
-export const deleteTPAByCatalogId = async (req, res) => {
-    const { catalog_id } = req.params;
-    const filePath = join(__dirname, "../tpa", `tpa-${catalog_id}.json`);
-  
-    try {
-      await fs.unlink(filePath);
-      res.send("TPA eliminado con éxito");
-    } catch (err) {
-      if (err.code === 'ENOENT') {
-        res.status(404).send("El TPA no existe");
-      } else {
-        console.error("Error al eliminar el archivo:", err);
-        res.status(500).send("Error al eliminar el TPA");
-      }
-    }
-  };
 
 export const updateCatalog = async (req, res) => {
-    const {id} = req.params
-    const {name, startDate, endDate, dashboard_id} = req.body
+    const { id } = req.params;
+    const { name, startDate, endDate, dashboard_id } = req.body;
 
     const currentCatalog = await models.Catalog.findByPk(id);
     if (!currentCatalog) {
-        return res.status(404).json({ message: 'Catalog not found' });
+        return res.status(404).json({ message: "Catalog not found" });
     }
 
-    await models.Catalog.update({
-        name,
-        startDate,
-        endDate,
-        dashboard_id,
-    }, {
-        where: {
-            id
+    await models.Catalog.update(
+        {
+            name,
+            startDate,
+            endDate,
+            dashboard_id,
+        },
+        {
+            where: {
+                id,
+            },
         }
-    });
+    );
 
     const row = await models.Catalog.findByPk(id);
 
     res.json(row);
-}
+};
 
 export const deleteCatalog = async (req, res) => {
     const result = await models.Catalog.destroy({
         where: {
-            id: req.params.id
-        }
+            id: req.params.id,
+        },
     });
 
     if (result <= 0)
         return res.status(404).json({
-          message: "Catalog not found",
+            message: "Catalog not found",
         });
-    
+
     res.sendStatus(204);
-}
+};
--- src/routes/catalog.routes.js
+++ src/routes/catalog.routes.js
@@ -1,13 +1,10 @@
 import { Router } from "express";
 import {
-  getCatalogs,
-  getCatalog,
-  createCatalog,
-  getTpa,
-  saveTpa,
-  deleteTPAByCatalogId,
-  updateCatalog,
-  deleteCatalog,
+    getCatalogs,
+    getCatalog,
+    createCatalog,
+    updateCatalog,
+    deleteCatalog,
 } from "../controllers/catalog.controller.js";
 
 const router = Router();
@@ -18,11 +15,6 @@ router.get("/catalogs/:id", getCatalog);
 router.post("/catalogs", createCatalog);
 router.patch("/catalogs/:id", updateCatalog);
 router.delete("/catalogs/:id", deleteCatalog);
-
-// TPAs
-router.get("/catalogs/:catalog_id/tpa", getTpa);
-router.post("/catalogs/tpa", saveTpa);
-router.delete("/catalogs/:catalog_id/tpa", deleteTPAByCatalogId);
 
 export default router;
 
