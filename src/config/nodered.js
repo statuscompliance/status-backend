ccb091d5146154097c90a6109dc414e8b8ce136e
[[alvarobernal2412]] <alvarobc2412@gmail.com>
2025-02-05 12:48:16 +0100

    fix(auth): added credentials in nodered req
    I have included a header to handle the communication between nodered and status-backend

--- src/config/nodered.js
+++ src/config/nodered.js
@@ -1,12 +1,19 @@
 import axios from 'axios';
 
 const nodeRedUrl = process.env.NODE_RED_URL;
+const nodeRedUsername = process.env.USER_STATUS;
+const nodeRedPassword = process.env.PASS_STATUS;
 
 const nodered = axios.create({
   baseURL: nodeRedUrl,
   timeout: 3000,
   headers: {
     'Content-Type': 'application/json',
+    withCredentials: true,
+  },
+  auth: {
+    username: nodeRedUsername,
+    password: nodeRedPassword,
   },
 });
 
