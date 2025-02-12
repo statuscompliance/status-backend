add2344f206d98c1ccacc617683f3be07b6b0573
[[alvarobernal2412]] <alvarobc2412@gmail.com>
2025-02-12 17:51:47 +0100

    feat: added grafana points entity

--- src/utils/storeGuaranteePoints.js
+++ src/utils/storeGuaranteePoints.js
@@ -4,42 +4,65 @@ import { getDates } from './dates.js';
 import registry from '../config/registry.js';
 
 export async function storeGuaranteePoints(guaranteeStates, agreementId) {
-  try {
-    const points = [];
+  const result = { storedPoints: [], error: [] };
 
-    console.log(guaranteeStates);
+  const points = [];
 
-    for (const guaranteeResult of guaranteeStates) {
-      const agreementResponse = await registry.get(`api/v6/agreements/${agreementId}`);
-      const agreement = agreementResponse.data;
-      const guaranteeTerm = agreement.terms.guarantees.find(x => x.id === guaranteeResult.id);
-      if (!guaranteeTerm) continue;
+  for (const guaranteeResult of guaranteeStates) {
+    const agreementResponse = await registry.get(`api/v6/agreements/${agreementId}`);
+    const agreement = agreementResponse.data;
+    const guaranteeTerm = agreement.terms.guarantees.find(x => x.id === guaranteeResult.id);
+    if (!guaranteeTerm) continue;
 
-      const guaranteeResultWindow = guaranteeTerm.of[0].window;
-      guaranteeResultWindow.from = getDates(
-        new Date(guaranteeResult.period.from), 
-        new Date(guaranteeResult.period.to), 
-        guaranteeResultWindow.period || 'monthly'
-      );
-      guaranteeResultWindow.from = guaranteeResultWindow.from[guaranteeResultWindow.from.length - 1];
+    const guaranteeResultWindow = guaranteeTerm.of[0].window;
+    guaranteeResultWindow.from = getDates(
+      new Date(guaranteeResult.period.from), 
+      new Date(guaranteeResult.period.to), 
+      guaranteeResultWindow.period || 'monthly'
+    );
+    guaranteeResultWindow.from = guaranteeResultWindow.from[guaranteeResultWindow.from.length - 1];
 
-      const timestamp = moment(guaranteeResultWindow.from).toISOString();
-      const pointData = {
-        agreementId: guaranteeResult.agreementId,
-        guaranteeId: guaranteeResult.id,
-        guaranteeValue: Object.values(guaranteeResult.metrics)[0],
-        guaranteeResult: guaranteeResult.value,
-        timestamp: timestamp,
-        metrics: guaranteeResult.metrics,
-        scope: guaranteeResult.scope
-      };
+    const timestamp = moment(guaranteeResultWindow.from).toISOString();
+    const computationGroup = guaranteeResult.evidences[0]?.computationGroup;
+    console.log(`Computation Group: ${computationGroup}`);
+    const pointData = {
+      agreementId: guaranteeResult.agreementId,
+      guaranteeId: guaranteeResult.id,
+      guaranteeValue: Object.values(guaranteeResult.metrics)[0],
+      guaranteeResult: guaranteeResult.value,
+      timestamp: timestamp,
+      metrics: guaranteeResult.metrics,
+      scope: guaranteeResult.scope,
+      computationGroup: computationGroup
+    };
 
+    const existingPoint = await models.Point.findOne({
+      where: {
+        timestamp: pointData.timestamp,
+        scope: pointData.scope,
+        guaranteeId: pointData.guaranteeId,
+        agreementId: pointData.agreementId
+      }
+    });
+
+    console.log(`existingPoint: ${JSON.stringify(existingPoint,null, 2)}`);
+
+    if (existingPoint !== null) {
+      result.error.push({ message: `Point with \n timestamp: ${pointData.timestamp},\n scope: \n ${JSON.stringify(pointData.scope)},\n guaranteeId: ${pointData.guaranteeId},\n and agreementId: ${pointData.agreementId}\n already exists.` });
+      console.log(`[points] Point with \n timestamp: ${pointData.timestamp},\n scope: \n ${JSON.stringify(pointData.scope)},\n guaranteeId: ${pointData.guaranteeId},\n and agreementId: ${pointData.agreementId}\n already exists.`);
+    } else {
       points.push(pointData);
     }
+
+  }
+
+  try {
     console.log(`[points] ${points.length} guarantee points successfully stored in PostgreSQL.`);
-    const storedPoints = await models.Point.bulkCreate(points);
-    return storedPoints;
+    result.storedPoints = await models.Point.bulkCreate(points);
   } catch (error) {
     console.error('Error storing guarantee points:', error);
+    result.error.push({ message: 'Error storing guarantee points', details: error });
   }
+
+  return result;
 }
