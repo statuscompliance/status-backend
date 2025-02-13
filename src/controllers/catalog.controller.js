ceef57e11af643d9049d348b53ad9be1ff7b3026
[[alvarobernal2412]] <alvarobc2412@gmail.com>
2025-02-13 10:52:52 +0100

    feat: added agreementTranslator initial version

--- src/controllers/catalog.controller.js
+++ src/controllers/catalog.controller.js
@@ -1,4 +1,7 @@
 import models from '../../db/models.js';
+import { storeGuaranteePoints } from '../utils/storeGuaranteePoints.js';
+import registry from '../config/registry.js';
+import { createAgreement } from '../utils/agreementTranslator.js';
 
 export const getCatalogs = async (req, res) => {
   const catalogs = await models.Catalog.findAll();
@@ -34,31 +37,35 @@ export const createCatalog = async (req, res) => {
 };
 
 export const updateCatalog = async (req, res) => {
-  const { id } = req.params;
-  const { name, startDate, endDate, dashboard_id } = req.body;
+  try {
+    const { id } = req.params;
+    const { name, startDate, endDate, dashboard_id, tpaId } = req.body;
 
-  const currentCatalog = await models.Catalog.findByPk(id);
-  if (!currentCatalog) {
-    return res.status(404).json({ message: 'Catalog not found' });
-  }
-
-  await models.Catalog.update(
-    {
-      name,
-      startDate,
-      endDate,
-      dashboard_id,
-    },
-    {
-      where: {
-        id,
-      },
+    const currentCatalog = await models.Catalog.findByPk(id);
+    if (!currentCatalog) {
+      return res.status(404).json({ message: 'Catalog not found' });
     }
-  );
 
-  const row = await models.Catalog.findByPk(id);
-
-  res.json(row);
+    const updatedCatalog = await models.Catalog.update(
+      {
+        name,
+        startDate,
+        endDate,
+        dashboard_id,
+        tpaId,
+      },
+      {
+        where: {
+          id,
+        },
+        returning: true,
+        plain: true,
+      }
+    );
+    res.status(200).json(updatedCatalog[1]); // The first element is the number of affectedRows
+  } catch (error) {
+    res.status(500).json({ message: `Failed to update catalog, error: ${error.message}` });
+  }
 };
 
 export const deleteCatalog = async (req, res) => {
@@ -75,3 +82,38 @@ export const deleteCatalog = async (req, res) => {
 
   res.sendStatus(204);
 };
+
+export async function calculatePoints(req, res) {
+  try {
+    const agreementId = req.params.tpaId;
+    const { from, to } = req.query;
+    //In this point, the catalog should be translated to agreement
+
+    const catalog = await models.Catalog.findOne({ where: {tpaId: agreementId}});
+    //Get controls by catalogId
+    const controls = await models.Control.findAll({where: {catalogId: catalog.id}});
+
+    const agreement = createAgreement(catalog, controls,{}, {}); // Update the Agreement or Create it ?
+    
+    console.log('Agreement \n', JSON.stringify(agreement, null, 2));
+    const guaranteesStates = await registry.get(`api/v6/states/${agreementId}/guarantees`, {
+      params: { from, to, newPeriodsFromGuarantees: false },
+      headers: { 'x-access-token': req.cookies.accessToken }
+    });
+    const { storedPoints, error } = await storeGuaranteePoints(guaranteesStates.data, agreementId);
+    if (error.length > 0) {
+      const points = await models.Point.findAll({where: {agreementId}});
+      if (points.length > 0) {
+        res.status(200).json(points);
+      } else {
+        res.status(400).json(error);
+      }
+    } else {
+      res.status(200).json(storedPoints);
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: `Failed to get points, error: ${error.message}`,
+    });
+  }
+}
