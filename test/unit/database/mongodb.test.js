b271bb7c36e70eb109143a2891965bb1d81b68a6
idavid80 <dominguezbueno.david@gmail.com>
2025-03-19 19:53:31 +0100

    modified: test/unit/database/mongodb.test.js

--- test/unit/database/mongodb.test.js
+++ test/unit/database/mongodb.test.js
@@ -30,24 +30,24 @@ describe("Database Connections", () => {
   describe('MongoDB Connection and Operations', () => {
     it('inserts, updates, and deletes a document', async () => {
       // Create a new user in MongoDB
-      const createdUser = await UserMongo.create({ username: 'testuser', password: '$2a$12$/C3Rl8zpVElZxroDRQZjEe.PWGKUeMdZCWZo8LaJUS6HlKKDkikge' });
+      const createdUser = await UserMongo.create({ username: 'testuser',  password: process.env.TEST_USER_PASSWORD || 'defaultpassword' });
       
       // Verify that the user was created correctly
       expect(createdUser.username).toBe('testuser');
-      expect(createdUser.password).toBe('$2a$12$/C3Rl8zpVElZxroDRQZjEe.PWGKUeMdZCWZo8LaJUS6HlKKDkikge');
+      expect(createdUser.password).toBe('defaultpassword');
 
       // Find the created user in the database
       const foundUser = await UserMongo.findOne({ username: 'testuser' });
       expect(foundUser).not.toBeNull();
       expect(foundUser.username).toBe('testuser');
-      expect(foundUser.password).toBe('$2a$12$/C3Rl8zpVElZxroDRQZjEe.PWGKUeMdZCWZo8LaJUS6HlKKDkikge');
+      expect(foundUser.password).toBe('defaultpassword');
 
       // Update the user's password
-      await UserMongo.updateOne({ username: 'testuser' }, { $set: { password: '$2a$12$WmML06uj4BEmDzexfa0zRuIFtNpe6gkgjl3ozRFwbhcY6FzfVNrC.' } });
+      await UserMongo.updateOne({ username: 'testuser' }, { $set: { password: process.env.TEST_USER_PASSWORD || 'newPassword' }});
 
       // Verify that the password has been updated
       const updatedUser = await UserMongo.findOne({ username: 'testuser' });
-      expect(updatedUser.password).toBe('$2a$12$WmML06uj4BEmDzexfa0zRuIFtNpe6gkgjl3ozRFwbhcY6FzfVNrC.');
+      expect(updatedUser.password).toBe('newPassword');
 
       // Delete the user from the database
       await UserMongo.deleteMany({ username: 'testuser' });
