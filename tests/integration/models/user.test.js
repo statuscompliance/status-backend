c025112c7269eb8f3a75c5c7aeb25ba598b00bed
[[alvarobernal2412]] <alvarobc2412@gmail.com>
2025-03-27 17:16:17 +0100

    chore(test): added integration test placeholder

---
+++ tests/integration/models/user.test.js
@@ -0,0 +1,9 @@
+import { expect, describe, it, } from 'vitest';
+
+describe('User models', () => {
+  describe('Always pass', () => { // This is a placeholder test
+    it('should pass', () => {
+      expect(true).toBe(true);
+    });
+  });
+});
