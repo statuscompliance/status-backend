1a1e7fc83e113052b54ac578d926790b89e8bc40
idavid80 <dominguezbueno.david@gmail.com>
2025-03-31 12:58:19 +0200

    test: update veryfyAmin and  add verifyAdmin middleware tests

--- src/middleware/verifyAdmin.js
+++ src/middleware/verifyAdmin.js
@@ -1,23 +1,21 @@
-import jwt from 'jsonwebtoken';
+import { verifyAccessToken } from '../utils/tokenUtils';
 
-export function verifyAdmin(req, res, next) {
-  const accessToken = req.headers['x-access-token'] ?? req.cookies?.accessToken;
-  if (!accessToken) {
+export async function verifyAdmin(req, res, next) {
+  // Verify token
+  const { decoded, error } = await verifyAccessToken(req.headers['x-access-token']);
+  
+  // If the token is not found, reply with a 401 error.
+  if (!req.headers['x-access-token']) {
     return res.status(401).json({ message: 'No token provided' });
   }
-
-  try {
-    // Verify token
-    const decoded = jwt.verify(accessToken, process.env.JWT_SECRET);
-
-    // Authorize only ADMIN users
-    if (decoded.authority !== 'ADMIN') {
-      return res.status(403).json({ message: 'Forbidden' });
-    }
-    
-    req.user = decoded;
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: `Unauthorized: ${error.message}` });
+  // If error in the validation of the token.
+  if (error) {
+    console.error('Token verification failed:', error);
+    return res.status(401).json({ message: 'Unauthorized: Invalid token' });
   }
+  // If token not have a valid authority,
+  if (!decoded || decoded.authority !== 'ADMIN') {
+    return res.status(403).json({ message: 'Forbidden' });
+  }
+  next();
 }
