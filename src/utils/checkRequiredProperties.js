ad08457224ce32cf3fb9f3a15bc10855ae944ea2
tirirote <josefranciscoaguzabenitez@gmail.com>
2025-04-01 17:03:59 +0200

    tests: fixed the changes that alvaro requested

--- src/utils/checkRequiredProperties.js
+++ src/utils/checkRequiredProperties.js
@@ -2,7 +2,6 @@ export function checkRequiredProperties(obj, requiredProps) {
   if (!obj || typeof obj !== 'object') {
     return { validation: false, textError: 'Invalid object or missing required properties' };
   }
-  
   const missingProps = requiredProps.filter(prop => !Object.prototype.hasOwnProperty.call(obj, prop));
 
   if (missingProps.length > 0) {
