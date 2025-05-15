987f2021308a9129feccf6db5eab046bcee7db51
[[alvarobernal2412]] <alvarobc2412@gmail.com>
2025-05-15 10:20:52 +0200

    feat(grafana): added new panel templates

--- src/utils/panels/gaugeStructure.js
+++ src/utils/panels/gaugeStructure.js
@@ -1,7 +1,7 @@
 const gaugeStructure = {
   datasource: {
     default: true,
-    type: 'mysql',
+    type: 'postgres',
     uid: 'P5E4ECD82955BB660',
   },
   description: '',
@@ -66,56 +66,48 @@ const gaugeStructure = {
   pluginVersion: '11.2.1',
   targets: [
     {
-      dataset: 'statusdb',
       datasource: {
-        type: 'mysql',
+        type: 'grafana-postgresql-datasource',
         uid: 'P5E4ECD82955BB660',
       },
       editorMode: 'builder',
       format: 'table',
       rawQuery: true,
-      rawSql: 'SELECT COUNT(`limit`) FROM statusdb.Configurations WHERE `limit` = 1 LIMIT 50 ',
+      rawSql: 'SELECT "agreementId", "guaranteeValue", "timestamp" FROM "Points"',
       refId: 'A',
       sql: {
         columns: [
           {
-            name: 'COUNT',
             parameters: [
               {
-                name: '`limit`',
+                name: '"agreementId"',
                 type: 'functionParameter',
               },
             ],
             type: 'function',
+          },{
+            parameters: [
+              {
+                'name': '"guaranteeValue"',
+                'type': 'functionParameter'
+              }
+            ],
+            type: 'function'
           },
+          {
+            parameters: [
+              {
+                'name': '"timestamp"',
+                'type': 'functionParameter'
+              }
+            ],
+            type: 'function'
+          }
         ],
         groupBy: [],
         limit: 50,
-        whereJsonTree: {
-          children1: [
-            {
-              id: 'aabbbb8b-0123-4456-b89a-b192488a102c',
-              properties: {
-                field: '`limit`',
-                fieldSrc: 'field',
-                operator: 'equal',
-                value: [1],
-                valueError: [null],
-                valueSrc: ['value'],
-                valueType: ['number'],
-              },
-              type: 'rule',
-            },
-          ],
-          id: '998a889b-0123-4456-b89a-b1924882e862',
-          properties: {
-            conjunction: 'AND',
-          },
-          type: 'group',
-        },
-        whereString: '`limit` = 1',
       },
-      table: 'Configurations',
+      table: '"Points"',
     },
   ],
   title: 'Panel Title',
