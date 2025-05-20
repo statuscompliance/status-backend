67361e20824d09567bd86c66fe11465fc861ecb5
[[alvarobernal2412]] <alvarobc2412@gmail.com>
2025-05-20 13:13:13 +0200

    test(logger): added logger and clean-logs modules

--- src/config/load-env.js
+++ src/config/load-env.js
@@ -1,5 +1,6 @@
-// load-env.js
+// load-env.js  /* istanbul ignore if */
 if (!import.meta.env?.VITEST) {
   await import('dotenv/config');
 }
+/* istanbul ignore next */
 await import('../index.js');
