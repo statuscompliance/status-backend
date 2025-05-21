aa301b59ee819cb6cdd79be078359e32cf0c603f
[[alvarobernal2412]] <alvarobc2412@gmail.com>
2025-05-21 11:42:47 +0200

    feat(logger): add database log level

--- src/db/database.js
+++ src/db/database.js
@@ -15,7 +15,7 @@ const getPostgresConfig = async () => {
     password: process.env.DB_PASSWORD || 'root',
     database: process.env.DB_NAME || 'statusdb',
     ssl: false,
-    logging: (msg) => logger.debug(msg),
+    logging: (msg) => logger.database(msg),
   });
   logger.info('Connecting to Postgres...', { 
     host: process.env.DB_HOST || 'localhost',
